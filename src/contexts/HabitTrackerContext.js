e64cee40d20be28ee3318cc90914f3c35dbf35a4
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-07-16 00:18:31 +0530

    feat: add habit reducers

--- src/contexts/HabitTrackerContext.js
+++ src/contexts/HabitTrackerContext.js
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
+import { habits } from "../DB/habits";
 
 const HabitTrackerContext = createContext();
 
@@ -11,9 +12,71 @@ export const initialHabitDetails = {
   startDate: "",
 };
 
+const initialState = {
+  habits: habits,
+  habitDetails: initialHabitDetails,
+};
+
+const reducerFunc = (state, { type, payload }) => {
+  switch (type) {
+    case "ADD_HABIT":
+      return { ...state, habits: [...state.habits, payload] };
+    case "ADD_HABIT_DETAILS":
+      return {
+        ...state,
+        habitDetails: { ...state.habitDetails, [payload.name]: payload.value },
+      };
+    case "EDIT_HABIT":
+      return {
+        ...state,
+        habits: state.habits.map((habit) =>
+          habit.id === payload.id ? { ...payload } : habit
+        ),
+      };
+    case "EDIT_HABIT_DETAILS":
+      return { ...state, habitDetails: payload };
+    case "RESET_HABIT_DETAILS":
+      return { ...state, habitDetails: payload };
+    case "MOVE_TO_TRASH":
+      return {
+        ...state,
+        habits: state.habits.filter((habit) => habit.id !== payload.id),
+      };
+    case "MOVE_TO_ARCHIEVE":
+      return {
+        ...state,
+        habits: state.habits
+          .map((habit) =>
+            habit.id === payload.id
+              ? { ...habit, archieved: !habit.archieved }
+              : habit
+          )
+          .filter((habit) => habit.id !== payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
 export const HabitTrackerProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducerFunc, initialState);
+  const [openHabitModal, setOpenHabitModal] = useState(false);
+  const [editingHabitId, setEditingHabitId] = useState(null);
+  const alreadyArchieved = (habit) => habit.archieved;
   return (
-    <HabitTrackerContext.Provider>{children}</HabitTrackerContext.Provider>
+    <HabitTrackerContext.Provider
+      value={{
+        state,
+        dispatch,
+        alreadyArchieved,
+        openHabitModal,
+        setOpenHabitModal,
+        editingHabitId,
+        setEditingHabitId,
+      }}
+    >
+      {children}
+    </HabitTrackerContext.Provider>
   );
 };
 

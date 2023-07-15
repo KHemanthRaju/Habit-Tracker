22458e20333ad11f689057af5ba5860db61791fb
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-07-15 23:59:38 +0530

    feat: add habits

--- src/contexts/HabitTrackerContext.js
+++ src/contexts/HabitTrackerContext.js
@@ -2,6 +2,15 @@ import { createContext, useContext } from "react";
 
 const HabitTrackerContext = createContext();
 
+export const initialHabitDetails = {
+  id: "",
+  name: "",
+  repeat: "",
+  goal: "",
+  timeOfDay: "",
+  startDate: "",
+};
+
 export const HabitTrackerProvider = ({ children }) => {
   return (
     <HabitTrackerContext.Provider>{children}</HabitTrackerContext.Provider>

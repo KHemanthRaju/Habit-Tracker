bad03573e1db6b5bf784173e89cbff4947a3c484
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-07-16 01:04:35 +0530

    feat: add Home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,46 @@
+import { useNavigate } from "react-router-dom";
+import {
+  initialHabitDetails,
+  useHabits,
+} from "../../contexts/HabitTrackerContext";
+import { IoMdAddCircle } from "react-icons/io";
 export const HomePage = () => {
+  const navigate = useNavigate();
+  const { state, dispatch, openHabitModal, setOpenHabitModal } = useHabits();
+
   return (
     <>
-      <h2>Home Page</h2>
+      <div className="home">
+        <h1 className="title">Home</h1>
+        <hr />
+        <div className="habit-titles">
+          <div className="habit-title">
+            <div className="action-icons add">
+              <IoMdAddCircle
+                title="Create habit"
+                onClick={() => {
+                  setOpenHabitModal((prev) => !prev);
+                  dispatch({
+                    type: "ADD_HABIT_DETAILS",
+                    payload: initialHabitDetails,
+                  });
+                }}
+              />
+            </div>
+          </div>
+          {state?.habits?.map((habit) => (
+            <h2>{habit.name}</h2>
+          ))}
+        </div>
+        <div>
+          <button
+            className="archieve-page-btn"
+            onClick={() => navigate("/archieves")}
+          >
+            Go to Archives
+          </button>
+        </div>
+      </div>
     </>
   );
 };
